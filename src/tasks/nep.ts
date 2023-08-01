bd1a59c381c0d8473453dbb2f3020df52f8de951
Glup Shitto <frigley.kol@gmail.com>
2023-08-01 17:18:02 -0400

    Wish for ultrahydrated

--- src/tasks/nep.ts
+++ src/tasks/nep.ts
@@ -1,4 +1,5 @@
-import { $location, $monsters, $skill, Macro } from "libram";
+import { $effect, $item, $location, $monsters, $skill, have, Macro } from "libram";
+import { cliExecute } from "kolmafia";
 import { args } from "../args";
 import { turnsRemaining } from "../lib";
 import { baggoOutfit } from "../outfit";
@@ -7,6 +8,19 @@ import { BaggoCombatStrategy } from "../engine/combat";
 import { EFFECTS } from "../effects";
 
 export const NEP_TASKS: BaggoTask[] = [
+  {
+    name: `genie wish ultrahydrated`,
+    completed: () => have($effect`Ultrahydrated`),
+    do: (): void => {
+      cliExecute(`genie effect ultrahydrated`);
+    },
+    acquire: [
+      {
+        item: $item`pocket wish`,
+        price: 50000,
+      },
+    ],
+  },
   {
     name: "Collect filters",
     completed: () => turnsRemaining() <= 0 || args.buff,
