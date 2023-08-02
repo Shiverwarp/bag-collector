847094be8d89bb9151be3fa47a4c3a97026a8701
Shiverwarp <Lorenz69@gmail.com>
2023-08-02 04:53:20 -0600

    Check for goose before throwing on experience

--- src/tasks/nep.ts
+++ src/tasks/nep.ts
@@ -1,18 +1,18 @@
-import { $location, $monsters, $skill, Macro } from "libram";
+import { $familiar, $location, $monsters, $skill, Macro } from "libram";
 import { args } from "../args";
 import { turnsRemaining } from "../lib";
 import { baggoOutfit } from "../outfit";
 import { BaggoTask } from "../engine/task";
 import { BaggoCombatStrategy } from "../engine/combat";
 import { EFFECTS } from "../effects";
-import { Location, numericModifier } from "kolmafia";
+import { Location, myFamiliar, numericModifier } from "kolmafia";
 
 export const NEP_TASKS: BaggoTask[] = [
   {
     name: "Collect tatters",
     completed: () => turnsRemaining() <= 0 || args.buff,
     do: (): Location => {
-      if (numericModifier(`familiar experience`) < 10) {
+      if (myFamiliar() === $familiar`Grey Goose` && numericModifier(`familiar experience`) < 10) {
         throw "We have less than 10 familiar experience per adventure!";
       }
       return $location`The Haunted Library`;
